578271654b44fbb55792bfc6699a5bbb53892d95
czzzn <zczhang1994@gmail.com>
2024-06-27 19:06:35 +0000

    add cr rate

--- utils.py
+++ utils.py
@@ -1,1 +1,17 @@
 import numpy as np
+
+def compatible_relaxation_rate(e1,e2):
+    """
+    Params:
+    -----------
+    e1: vector of cr score from previous iteration
+    e2: vector of cr score from current iteration
+
+    Return:
+    -----------
+    rate: measures the "converge" of cr steps
+    """
+
+    return np.linalg.norm(e2)/np.linalg.norm(e1)
+
+
